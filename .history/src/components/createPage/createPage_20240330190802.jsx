db81f2feb4b89302dbaacb0badbdcf731776507e
Thomas Musial <th896493@dal.ca>
2024-03-30 21:23:10 -0300

    submission sheneigans

---
+++ .history/src/components/createPage/createPage_20240330190802.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import ImageUploader from '../imageUploader/imageUploader';
+import './createPage.css'
+
+const CreateText = ({ onSave }) => {
+  const [propertyData, setPropertyData] = useState({
+    propertyHerobanner: '',
+    propertyPrice: '',
+    propertyOpeningDate: '',
+    propertyClosingDate: '',
+    propertyMainDescription: '',
+    propertyDescription2: '',
+    propertyDescription3: '',
+    propertyVideo: '',
+    featureAttributes:'', 
+    sliderImages:'',
+    carouselImages1: '',
+    carouselImages2: '',
+    documents: '',
+    factsList: '',
+    profile: ''
+    // Add more properties as needed
+  });
+
+  const featureInputs = [
+    { name: 'Property Herobanner', key: 'property-herobanner', image:true },
+    { name: 'Property Price', key: 'property-price' },
+    { name: 'Property Opening Date', key: 'property-openingDate' },
+    { name: 'Property Closing Date', key: 'property-closingDate' },
+    { name: 'Property Main Description', key: 'property-mainDescription' },
+    { name: 'Property Description 2', key: 'property-description2' },
+    { name: 'Property Description 3', key: 'property-description3' },
+    { name: 'Property Video', key: 'propertyVideo' },
+    {name: 'Feature Attributes', key:'feature-attributes'},
+    {name:'Slider Images', key:'slider-images'},
+    {name:'Slider Images2', key:'slider-images2'},
+    {name:'Documents', key:'documents'},
+    {name:'Facts list', key:'facts-list'},
+    {name:'Profile', key:'profile'}
+
+
+    // Add more features as needed
+  ];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPropertyData({ ...propertyData, [name]: value });
+  };
+
+  const handleSave = () => {
+    onSave(propertyData);
+    // Optionally, you can reset the input fields after saving
+    setPropertyData({
+      propertyHerobanner: '',
+      propertyPrice: '',
+      propertyOpeningDate: '',
+      propertyClosingDate: '',
+      propertyMainDescription: '',
+      propertyDescription2: '',
+      propertyDescription3: '',
+      propertyVideo: '',
+      featureAttributes:'', 
+      sliderImages:'',
+      carouselImages1: '',
+      carouselImages2: '',
+      documents: '',
+      factsList: '',
+      profile: ''
+      // Add more properties as needed
+    });
+  };
+
+  return (
+    <div className='create-page-container'>
+      <h2>Create Property Text</h2>
+      {featureInputs.map((feature) => (
+
+        {feature.image ? (
+          <>
+          <ImageUploader/>
+          </>
+        ) :}
+
+        <div className='input-labels'
+        key={feature.key}>
+          <label htmlFor={feature.key}>{feature.name}:</label>
+          <input
+            type="text"
+            id={feature.key}
+            name={feature.key}
+            value={propertyData[feature.key]}
+            onChange={handleChange}
+            className='text-input'
+          />
+        </div>
+      ))}
+      <button onClick={handleSave}>Save</button>
+      <ImageUploader
+      multiple={true}/>
+    </div>
+  );
+};
+
+export default CreateText;
---
+++ .history/src/components/createPage/createPage_20240330204447.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import ImageUploader from '../imageUploader/imageUploader';
+import './createPage.css';
+
+const CreateText = ({ onSave }) => {
+
+  const [dataSubmitted, setDataSubmitted] = useState(false)
+  const [propertyData, setPropertyData] = useState({
+    propertyHerobanner: '',
+    propertyPrice: '',
+    propertyOpeningDate: '',
+    propertyClosingDate: '',
+    propertyMainDescription: '',
+    propertyDescription2: '',
+    propertyDescription3: '',
+    propertyVideo: '',
+    featureAttributes: '', 
+    sliderImages: '',
+    carouselImages1: '',
+    carouselImages2: '',
+    documents: '',
+    factsList: '',
+    profile: ''
+    // Add more properties as needed
+  });
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const featureInputs = [
+    { name: 'Property Herobanner', key: 'property-herobanner', image:true },
+    { name: 'Property Price', key: 'property-price' },
+    { name: 'Property Opening Date', key: 'property-openingDate' },
+    { name: 'Property Closing Date', key: 'property-closingDate' },
+    { name: 'Property Main Description', key: 'property-mainDescription' },
+    { name: 'Property Description 2', key: 'property-description2' },
+    { name: 'Property Description 3', key: 'property-description3' },
+    { name: 'Property Video', key: 'propertyVideo' },
+    { name: 'Feature Attributes', key: 'feature-attributes' },
+    { name: 'Slider Images', key: 'slider-images' },
+    { name: 'Slider Images2', key: 'slider-images2' },
+    { name: 'Documents', key: 'documents' },
+    { name: 'Facts list', key: 'facts-list' },
+    { name: 'Profile', key: 'profile' }
+    // Add more features as needed
+  ];
+
+  const handleChange = (e, featureKey) => {
+    const { name, value } = e.target;
+    if (featureKey) {
+      // If featureKey is provided, it means this is for a regular text input
+      setPropertyData({ ...propertyData, [featureKey]: value });
+    } else {
+      // Otherwise, it's for an image upload input
+      setPropertyData({ ...propertyData, [name]: e });
+    }
+  };
+  
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === featureInputs.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? featureInputs.length - 1 : prevIndex - 1));
+  };
+
+  const handleSave = () => {
+    // onSave(propertyData);
+    // Optionally, you can reset the input fields after saving
+    setPropertyData({
+      propertyHerobanner: '',
+      propertyPrice: '',
+      propertyOpeningDate: '',
+      propertyClosingDate: '',
+      propertyMainDescription: '',
+      propertyDescription2: '',
+      propertyDescription3: '',
+      propertyVideo: '',
+      featureAttributes: '', 
+      sliderImages: '',
+      carouselImages1: '',
+      carouselImages2: '',
+      documents: '',
+      factsList: '',
+      profile: ''
+      // Add more properties as needed
+    });
+
+    console.log('property data',propertyData)
+  };
+
+
+
+
+
+  return (
+    <>
+    <div className='create-page-container'>
+      <h2>Create Property Text</h2>
+      <div className='inputs'>
+        {featureInputs.map((feature, index) => (
+          <div key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+            {feature.image ? (
+              <ImageUploader inputName={feature.name} />
+            ) : (
+              <div className='input-labels'>
+                <label htmlFor={feature.key}>{feature.name}:</label>
+                <input
+                  type="text"
+                  id={feature.key}
+                  name={feature.key}
+                  value={propertyData[feature.key] || ''}
+                  onChange={(e) => handleChange(e, feature.key)} 
+                  className='text-input'
+                  placeholder='enter text here'
+                />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div>
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+      <button onClick={handleSave}>Save</button>
+    </div>
+
+
+
+    </>
+  );
+};
+
+export default CreateText;
