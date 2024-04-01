187b764a50280719359cafb93fb3f7d317f11859
Thomas Musial <th896493@dal.ca>
2024-04-01 13:44:55 -0300

    files should only upload once

---
+++ .history/src/components/createPage/createPage_20240401101638.jsx
@@ -0,0 +1,234 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useImageContext } from '../../context/imageContext';
+import ImageUploader from '../imageUploader/imageUploader';
+import Property from '../property/property';
+import './createPage.css';
+
+const CreateText = ({ onSave }) => {
+
+  const [dataSubmitted, setDataSubmitted] = useState(false)
+
+
+  
+
+
+  const [propertyPrice, setPropertyPrice] = useState(null);
+  const [propertyOpeningDate, setPropertyOpeningDate] = useState(null);
+  const [propertyClosingDate, setPropertyClosingDate] = useState(null);
+  const [propertyMainDescription, setPropertyMainDescription] = useState(null);
+  const [propertyDescription2, setPropertyDescription2] = useState(null);
+  const [propertyDescription3, setPropertyDescription3] = useState(null);
+  const [propertyVideo, setPropertyVideo] = useState(null);
+  const [featureAttributes, setFeatureAttributes] = useState(null);
+
+  const [sliderImages2, setSliderImages2] = useState(null);
+  const [documents, setDocuments] = useState(null);
+  const [factsList, setFactsList] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  const {propertyHerobanner, setPropertyHerobanner} = useImageContext()
+  const {sliderImages, setSliderImages} = useImageContext()
+
+
+
+  const handleInputChange = (event) => {
+
+    setPropertyPrice(event.target.value)
+  }
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const featureInputs = [
+    { name: 'Property Herobanner', key: 'property-herobanner',
+     image:true, setterFunction: setPropertyHerobanner, multipleImages:false,
+    imagesArray:null },
+    { name: 'Property Price', key: 'property-price' },
+    { name: 'Property Opening Date', key: 'property-openingDate' },
+    { name: 'Property Closing Date', key: 'property-closingDate' },
+    { name: 'Property Main Description', key: 'property-mainDescription' },
+    { name: 'Property Description 2', key: 'property-description2' },
+    { name: 'Property Description 3', key: 'property-description3' },
+    { name: 'Property Video', key: 'propertyVideo' },
+    { name: 'Feature Attributes', key: 'feature-attributes' },
+    { name: 'Slider Images', key: 'slider-images',
+  image:true, setterFunction: setSliderImages, imagesArray: sliderImages,
+multipleImages:true },
+    { name: 'Slider Images2', key: 'slider-images2' },
+    { name: 'Documents', key: 'documents' },
+    { name: 'Facts list', key: 'facts-list' },
+    { name: 'Profile', key: 'profile' }
+   
+  ];
+
+  const propertyState = {
+    propertyHerobanner,
+    propertyPrice,
+    propertyOpeningDate,
+    propertyClosingDate,
+    propertyMainDescription,
+    propertyDescription2,
+    propertyDescription3,
+    propertyVideo,
+    featureAttributes,
+    sliderImages,
+    sliderImages2,
+    documents,
+    factsList,
+    profile
+  };
+
+  // const handleChange = (e, featureKey) => {
+  //   const { name, value } = e.target;
+  //   if(featureKey){
+  //     console.log('this is the feature key',featureKey)
+  //     console.log('the value is',e.target.value)
+  //   }
+  // };
+
+  const handleChange = (e, key) => {
+    switch (key) {
+      case 'property-herobanner':
+        setPropertyHerobanner(e.target.value);
+        break;
+      case 'property-price':
+        setPropertyPrice(e.target.value);
+        break;
+      case 'property-openingDate':
+        setPropertyOpeningDate(e.target.value);
+        break;
+      case 'property-closingDate':
+        setPropertyClosingDate(e.target.value);
+        break;
+      case 'property-mainDescription':
+        setPropertyMainDescription(e.target.value);
+        break;
+      case 'property-description2':
+        setPropertyDescription2(e.target.value);
+        break;
+      case 'property-description3':
+        setPropertyDescription3(e.target.value);
+        break;
+      case 'propertyVideo':
+        setPropertyVideo(e.target.value);
+        break;
+      case 'feature-attributes':
+        setFeatureAttributes(e.target.value);
+        break;
+      case 'slider-images':
+        setSliderImages(e.target.value);
+        break;
+      case 'slider-images2':
+        setSliderImages2(e.target.value);
+        break;
+      case 'documents':
+        setDocuments(e.target.value);
+        break;
+      case 'facts-list':
+        setFactsList(e.target.value);
+        break;
+      case 'profile':
+        setProfile(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+  
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === featureInputs.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? featureInputs.length - 1 : prevIndex - 1));
+  };
+
+  const handleSave = () => {
+    // Update propertyData with the extracted values
+   
+  
+   
+  
+    setDataSubmitted(true);
+  
+    // console.log('property data', propertyData);
+  };
+  
+ 
+  
+  
+useEffect(()=>{
+  console.log('the sloider list is',sliderImages)
+
+},[sliderImages])
+
+useEffect(()=>{
+  console.log('herobanner is:',propertyHerobanner)
+},[propertyHerobanner])
+  
+
+
+
+
+
+  return (
+    <>
+    <div className='create-page-container'>
+      <h2>Create Property Text</h2>
+      <div className='inputs'>
+        {featureInputs.map((feature, index) => (
+          <div key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+            {feature.image ? (
+              <ImageUploader inputName={feature.name}
+              setterFunction={feature.setterFunction}
+              multiple={feature.multipleImages}
+              imageArray={feature.imageArray} />
+            ) : (
+              <div className='input-labels'>
+                <label htmlFor={feature.key}>{feature.name}:</label>
+                <input
+                  type="text"
+                  id={feature.key}
+                  name={feature.key}
+                
+                  onChange={(e) => handleChange(e, feature.key)} 
+                  className='text-input'
+                  // placeholder='enter text here'
+                />
+              </div>
+            )}
+             {/* <button onClick={() => handleSaveIndividual(feature.key)}>Save {feature.name}</button> */}
+
+          </div>
+          
+        ))}
+      </div>
+      <div>
+
+      {/* <label htmlFor="propertyPrice">Property Price:</label>
+      <input
+        type="number"
+        id="propertyPrice"
+        value={propertyPrice}
+        onChange={handleInputChange}
+      /> */}
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+    
+      <button onClick={handleSave}>Create page</button>
+    </div>
+
+{dataSubmitted && (
+  <Property
+  
+  {...propertyState}
+  />
+)}
+
+    </>
+  );
+};
+
+export default CreateText;
---
+++ .history/src/components/imageUploader/imageUploader_20240401103133.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import { useImageContext } from '../../context/imageContext';
+import './imageUploader.css';
+
+const ImageUploader = ({ onSingleImageUpload, onMultipleImagesUpload, multiple,
+inputName, setterFunction, imageArray }) => {
+
+
+
+  const [droppedImages, setDroppedImages] = useState([]);
+
+  // Prevent default behavior for dragover and drop events
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  // Handle the dropped image files
+  const handleDrop = (event) => {
+    event.preventDefault();
+
+    // Access the dropped files from the event
+    const files = event.dataTransfer.files;
+
+    // Convert FileList to array
+    const fileList = Array.from(files);
+
+    // Read dropped image files as URLs
+    const readerPromises = fileList.map((file) => {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          resolve(reader.result); // Resolve with the data URL
+        };
+        reader.readAsDataURL(file);
+      });
+    });
+
+    // Set dropped images state with the resolved data URLs
+    Promise.all(readerPromises).then((imageURLs) => {
+      setDroppedImages((prevImages) => [...prevImages, ...imageURLs]);
+    });
+
+    if (multiple && setterFunction && fileList && fileList.length > 0) {
+      setterFunction((imageArray) => [...prevImages, ...fileList]); 
+      // Append the dropped files to the array
+    } else if (!multiple && setterFunction && fileList.length > 0) {
+      // If multiple is false and setterFunction is provided, update with the first file
+      setterFunction(fileList[0]); 
+    }
+  ;
+
+ 
+
+    // Invoke the appropriate upload function based on the multiple prop
+    if (multiple && onMultipleImagesUpload) {
+      onMultipleImagesUpload(fileList);
+    } else if (!multiple && onSingleImageUpload && fileList.length > 0) { // Only upload if files exist
+      onSingleImageUpload(fileList[0]); // Upload only the first file
+    }
+  };
+
+  useEffect(() => {
+    if (multiple && droppedImages.length > 0 && setterFunction) {
+      setterFunction(imageArray => [...imageArray, droppedImages]);
+      
+    } else if (!multiple && droppedImages.length > 0 && setterFunction) {
+      setterFunction(droppedImages[0]);
+    }
+  }, [droppedImages, multiple, setterFunction]);
+
+  return (
+    <div
+      className="image-uploader"
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
+
+{multiple ? (
+  droppedImages.map((imageURL, index) => (
+    <div key={index} className="image-preview">
+      <img src={imageURL} alt={`Dropped ${index + 1}`} />
+    </div>
+  ))
+) : (
+  droppedImages.length > 0 && (
+    <div className="image-preview">
+      <img src={droppedImages[droppedImages.length - 1]} alt="Single dropped image" />
+    </div>
+  )
+)}
+  
+
+    
+      <p>Drag and drop images here or click to upload {inputName}</p>
+      <input
+        type="file"
+        accept="image/*"
+        multiple={multiple}
+        onChange={(e) => {
+          const files = e.target.files;
+          const fileList = Array.from(files);
+          const readerPromises = fileList.map((file) => {
+            return new Promise((resolve) => {
+              const reader = new FileReader();
+              reader.onload = () => {
+                resolve(reader.result); // Resolve with the data URL
+              };
+              reader.readAsDataURL(file);
+            });
+          });
+          // Set dropped images state with the resolved data URLs
+          Promise.all(readerPromises).then((imageURLs) => {
+            setDroppedImages((prevImages) => [...prevImages, ...imageURLs]);
+          });
+
+          // Invoke the appropriate upload function based on the multiple prop
+          if (multiple && onMultipleImagesUpload) {
+            onMultipleImagesUpload(fileList);
+          } else if (!multiple && onSingleImageUpload && fileList.length > 0) { // Only upload if files exist
+            onSingleImageUpload(fileList[0]); // Upload only the first file
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default ImageUploader;
