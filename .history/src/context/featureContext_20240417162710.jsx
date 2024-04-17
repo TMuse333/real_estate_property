bfbce78d94cd2c7cee0633633ee4fb6da714ac71
Thomas Musial <th896493@dal.ca>
2024-04-17 16:43:46 -0300

    going to have to handle how to add the uploaded image to the featureList array from imageUploader component

---
+++ .history/src/context/featureContext_20240417162710.jsx
@@ -0,0 +1,71 @@
+import React, { createContext, useContext, useState } from "react";
+
+
+const FeatureContext = createContext()
+
+export const FeatureProvider = ({ children }) => {
+
+ 
+
+    const [featureListLength, setFeatureListLength] = useState(0)
+
+    const [featureList, setFeatureList] = useState([])
+
+    const handleInputChange = (index, value) => {
+      // Create a new feature object with the input value
+      const newFeature = { name: value };
+
+      // Update the featureList state with the new feature object
+      setFeatureList(prevList => {
+          // Copy the existing featureList array
+          const newList = [...prevList];
+          // Replace the item at the specified index with the new feature object
+          newList[index] = newFeature;
+          // Return the updated list
+          return newList;
+      });
+  };
+
+
+  const handleAddImage = (index, value) => {
+    // Create a new feature object with the input value
+    const newFeature = { image: value };
+
+    // Update the featureList state with the new feature object
+    setFeatureList(prevList => {
+        // Copy the existing featureList array
+        const newList = [...prevList];
+        // Replace the item at the specified index with the new feature object
+        newList[index] = newFeature;
+        // Return the updated list
+        return newList;
+    });
+};
+
+
+
+   
+    const contextValue = {
+            featureList,setFeatureListLength,
+            setFeatureList, featureListLength,
+            handleInputChange,
+            handleAddImage
+    }
+
+    useEffect
+  
+    return (
+      <FeatureContext.Provider value={contextValue}>
+        {children}
+      </FeatureContext.Provider>
+    );
+  };
+  
+  export const useFeatureContext = () => {
+    const context = useContext(FeatureContext);
+  
+    if (!context) {
+      throw new Error("useProfileContext must be used within a ProfileProvider");
+    }
+    return context;
+  };
